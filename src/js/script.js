f18be65c0148186d46083dd8398521bc9839a486
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2022-11-15 19:19:12 +0100

    Update dark mode styles

--- src/js/script.js
+++ src/js/script.js
@@ -12,6 +12,7 @@ changeLanguageCheckbox.addEventListener('change', (e) => {
 let languageIsEnNl = false;
 let verbs = [];
 
+addCurrentThemeClass();
 populateAllCards().then();
 
 async function populateAllCards() {
@@ -89,13 +90,7 @@ function setCardBackText(card, text) {
 }
 
 function switchDarkMode() {
-    if (!document.documentElement.classList.contains('light') && !document.documentElement.classList.contains('dark')) {
-        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-            document.documentElement.classList.add('dark');
-        } else {
-            document.documentElement.classList.add('light');
-        }
-    }
+    addCurrentThemeClass();
 
     if (document.documentElement.classList.contains('light')) {
         document.documentElement.classList.remove('light');
@@ -103,5 +98,15 @@ function switchDarkMode() {
     } else if (document.documentElement.classList.contains('dark')) {
         document.documentElement.classList.remove('dark');
         document.documentElement.classList.add('light');
+    }
+}
+
+function addCurrentThemeClass() {
+    if (!document.documentElement.classList.contains('light') && !document.documentElement.classList.contains('dark')) {
+        if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.add('light');
+        }
     }
 }
