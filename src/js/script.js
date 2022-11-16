bbf2c969b6e3b34164e3905ff185aee42337e72d
Anatoly Cherenkov <anatoly.cherenkov@gmail.com>
2022-11-16 14:33:58 +0100

    Refactor control icons

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,4 @@
-addCurrentThemeClass();
+updateTheme();
 
 const languageFromTo = {
     NlEn: "NL>EN",
@@ -24,29 +24,27 @@ function switchLanguage() {
 }
 
 function setLanguageLabel(caption) {
-    const label = document.getElementById('language-label');
+    const label = document.getElementById('change-language');
     label.innerText = caption;
 }
 
-function switchDarkMode() {
-    addCurrentThemeClass();
-
-    if (document.documentElement.classList.contains('light')) {
-        document.documentElement.classList.remove('light');
-        document.documentElement.classList.add('dark');
-    } else if (document.documentElement.classList.contains('dark')) {
-        document.documentElement.classList.remove('dark');
-        document.documentElement.classList.add('light');
-    }
-}
-
-function addCurrentThemeClass() {
+function updateTheme() {
     if (!document.documentElement.classList.contains('light') && !document.documentElement.classList.contains('dark')) {
         if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
             document.documentElement.classList.add('dark');
         } else {
             document.documentElement.classList.add('light');
         }
+    }
+}
+
+function switchTheme() {
+    if (document.documentElement.classList.contains('light')) {
+        document.documentElement.classList.remove('light');
+        document.documentElement.classList.add('dark');
+    } else if (document.documentElement.classList.contains('dark')) {
+        document.documentElement.classList.remove('dark');
+        document.documentElement.classList.add('light');
     }
 }
 
